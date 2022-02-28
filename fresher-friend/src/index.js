5a19d28fa1f8990c81d8da9d0da706fc5f4825b9
TEB018 <TEB018@student.bham.ac.uk>
2022-02-28 13:26:45 +0000

    Commented out account creation as it causes program not to compile

--- fresher-friend/src/index.js
+++ fresher-friend/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import AccountCreation from "./components/AccountCreation";
+//import AccountCreation from "./components/AccountCreation";
 
 ReactDOM.render(
   <React.StrictMode>
