aa10c3fdd55003aa9cc353eebca1cfca315bc8ee
reubenjsinclair <reubenjsinclair@gmail.com>
2022-02-28 11:00:53 +0000

    Revert index.js

--- fresher-friend/src/index.js
+++ fresher-friend/src/index.js
@@ -2,11 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import Login from "./components/Login";
 
 ReactDOM.render(
   <React.StrictMode>
-  <Login />
+  <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
