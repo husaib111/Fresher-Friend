adf9b7741af41b7f0e55ccccbccce50344e7caae
reubenjsinclair <reubenjsinclair@gmail.com>
2022-02-28 10:30:47 +0000

    Start login page

---
+++ fresher-friend/src/Login.js
@@ -0,0 +1,42 @@
+import "./Login.css";
+import React from 'react';
+import { GiThreeFriends } from "react-icons/gi";
+
+class Login extends React.Component {
+  render(){
+    return (
+      <div className="login">
+        <h1 className="logo">
+          <GiThreeFriends className="icon" />
+          Fresher Friend.
+        </h1>
+        <h2>Connecting UoB students</h2>
+        <form className="loginForm">
+          <div class="mb-3">
+            <input 
+              className="form-control loginInput"
+              type="email"
+              placeholder="example@student.bham.ac.uk"
+            />
+          </div>
+          <div class="mb-3">
+            <input
+              className="form-control loginInput"
+              type="password"
+              placeholder="Password"
+            />
+          </div>
+          <button type="submit" class="btn btn-primary">Log In</button>
+          <p><a href="#">Forgot password</a></p>
+        </form>
+        <hr/>
+        <div className="createAccount">
+          <p>New here?</p>
+          <button class="btn btn-primary">Create an Account</button>
+        </div>
+      </div>
+    );
+  } 
+}
+
+export default Login;
