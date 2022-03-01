75ce10ec63c448a2f9fefecffb72736789b8ddd0
reubenjsinclair <reubenjsinclair@gmail.com>
2022-03-01 23:16:32 +0000

    Rename folder to client

--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json()); //req.body
 
 app.listen(5001, () => {
-  console.log("Server started on port 5000.");
+  console.log("Server started on port 5001.");
 });
