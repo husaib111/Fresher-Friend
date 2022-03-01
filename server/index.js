212da6d7490ba50de2946ef3090daafcb31c8f6d
reubenjsinclair <reubenjsinclair@gmail.com>
2022-03-01 23:30:40 +0000

    Add base methods to server

--- server/index.js
+++ server/index.js
@@ -5,6 +5,38 @@ const cors = require("cors");
 app.use(cors());
 app.use(express.json()); //req.body
 
+app.get("",async(req,res) => {
+  try {
+    //
+  } catch (e) {
+    console.error(e.message);
+  }
+});
+
+app.post("",async(req,res) => {
+  try {
+    //
+  } catch (e) {
+    console.error(e.message);
+  }
+});
+
+app.put("",async(req,res) => {
+  try {
+    //
+  } catch (e) {
+    console.error(e.message);
+  }
+});
+
+app.delete("",async(req,res) => {
+  try {
+    //
+  } catch (e) {
+    console.error(e.message);
+  }
+});
+
 app.listen(5001, () => {
   console.log("Server started on port 5001.");
 });
