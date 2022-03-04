faa895b739ee9fb4708324cceaa36468da3b4b3d
reubenjsinclair <reubenjsinclair@gmail.com>
2022-03-04 14:35:48 +0000

    basic server

--- server/index.js
+++ server/index.js
@@ -1,15 +1,7 @@
-const express = require("express");
-const app = express();
-const cors = require("cors");
-const db = require("./queries");
-
-const port = 5001;
-
-app.use(cors());
-app.use(express.json()); //req.body
-
-app.listen(port, () => {
-  console.log("Server started on port %d", port);
-});
-
-app.get("/users", db.getUsers);
+#!/usr/bin/env nodejs
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+}).listen(8080, 'localhost');
+console.log('Server running at http://localhost:8080/');
