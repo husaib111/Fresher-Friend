55cca0c4e1305fd93a3c949bcff581198d9d9130
Tomas Grusz <grusztomas0@gmail.com>
2022-03-08 18:40:50 +0100

    Test auth 4

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -6,7 +6,7 @@ const Login = () => {
   async function loginUser(event) {
     event.preventDefault();
 
-    const response = await fetch("https://46.101.81.7:5001/login", {
+    const response = await fetch("https://46.101.81.7:5500/login", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
