2b3d0d994ca6cf13ff29efb31edb48ad5aec3d6f
Tomas Grusz <grusztomas0@gmail.com>
2022-03-08 18:51:44 +0100

    Test auth 6

--- server/auth.js
+++ server/auth.js
@@ -1,27 +1,6 @@
-const { check } = require("express-validator");
+const { response } = require("express");
 const db = require("./dbconnect");
 const jwt = require("jsonwebtoken");
-
-const loginFieldsCheck = check("email").custom(async (value, { req }) => {
-  const user = await db.query("SELECT * from users WHERE email = $1", [value]);
-
-  if (!user.rows.length) {
-    return res;
-  }
-
-  const validPassword = await compare(req.body.password, user.rows[0].password);
-
-  if (!validPassword) {
-    throw new Error("Wrong password");
-  }
-
-  req.user = user.rows[0];
-});
-
-module.exports = {
-  registerValidation: [email, password, emailExists],
-  loginValidation: [loginFieldsCheck],
-};
 
 const loginCheck = async (request, response) => {
   const { email, password } = request.body;
