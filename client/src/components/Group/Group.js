480bcb1c8e2e3bb8f5df6afab96d177e76db2b92
Husaib Hasan <husaib@hotmail.co.uk>
2022-03-09 17:20:04 +0000

    Basic chat UI added

--- client/src/components/Group/Group.js
+++ client/src/components/Group/Group.js
@@ -1,31 +1,83 @@
-// import { GiThreeFriends } from "react-icons/gi";
-// import React, {Component} from 'react';
-// import ProfileList from "./ProfileList";
-// import { useEffect, useState } from "react";
+import React, {useState } from "react";
+import ScrollToBottom from "react-scroll-to-bottom";
+import "./Group.css";
+
+function Group({ socket, username, room }) {
+
+  const [currentMessage, setCurrentMessage] = useState("");
+  const [messageList] = useState([]);
+  const [sendMessage] = useState("");
+
 
 /*
-class Group extends Component{
-    render(){
-        return(
-            <div className="App">
-                <h1 className="title">
-                    <GiThreeFriends className="icon" />
-                    Group Page
-                </h1>
-                <ProfileList/>
-            </div>
-        )
+  const sendMessage = async () => {
+    if (currentMessage !== "") {
+      const messageData = {
+        room: room,
+        author: username,
+        message: currentMessage,
+        time:
+          new Date(Date.now()).getHours() +
+          ":" +
+          new Date(Date.now()).getMinutes(),
+      };
+
+      await socket.emit("send_message", messageData);
+      setMessageList((list) => [...list, messageData]);
+      setCurrentMessage("");
     }
-}
+  };
+
+  useEffect(() => {
+    socket.on("receive_message", (data) => {
+      setMessageList((list) => [...list, data]);
+    });
+  }, [socket]);
 */
+  return (
+    <div className="chat-window">
+      <div className="chat-header">
+        <p>Live Chat</p>
+      </div>
+      <div className="chat-body">
+        <ScrollToBottom className="message-container">
+          {messageList.map((messageContent) => {
+            return (
+              <div
+                className="message"
+                id={username === messageContent.author ? "you" : "other"}
+              >
+                <div>
+                  <div className="message-content">
+                    <p>{messageContent.message}</p>
+                  </div>
+                  <div className="message-meta">
+                    <p id="time">{messageContent.time}</p>
+                    <p id="author">{messageContent.author}</p>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </ScrollToBottom>
+      </div>
+      <div className="chat-footer">
+        <input
+          type="text"
+          value={currentMessage}
+          placeholder="Hey..."
+          onChange={(event) => {
+            setCurrentMessage(event.target.value);
+          }}
+          onKeyPress={(event) => {
+            event.key === "Enter" && sendMessage();
+          }}
+        />
+        <button onClick={sendMessage}>&#9658;</button>
+      </div>
+    </div>
+  );
+};
 
-// const SingleChat = ({ fetchAgain, setFetchAgain }) => {
-//     const [messages, setMessages] = useState([]);
-//     const [loading, setLoading] = useState(false);
-//     const [newMessage, setNewMessage] = useState("");
-//     const [socketConnected, setSocketConnected] = useState(false);
-//     const [typing, setTyping] = useState(false);
-//     const [istyping, setIsTyping] = useState(false);
-//     };
+export default Group;
 
-// export default Group;
