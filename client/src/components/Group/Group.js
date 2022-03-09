1f0db859a5d598118334051cd41402cb5da51489
Tomas Grusz <grusztomas0@gmail.com>
2022-03-09 10:07:23 +0100

    Fix warnings in Group.js to enable pipeline

--- client/src/components/Group/Group.js
+++ client/src/components/Group/Group.js
@@ -1,8 +1,7 @@
-import { GiThreeFriends } from "react-icons/gi";
-import React, {Component} from 'react';
-import ProfileList from "./ProfileList";
-import { useEffect, useState } from "react";
-
+// import { GiThreeFriends } from "react-icons/gi";
+// import React, {Component} from 'react';
+// import ProfileList from "./ProfileList";
+// import { useEffect, useState } from "react";
 
 /*
 class Group extends Component{
@@ -20,13 +19,13 @@ class Group extends Component{
 }
 */
 
-const SingleChat = ({ fetchAgain, setFetchAgain }) => {
-    const [messages, setMessages] = useState([]);
-    const [loading, setLoading] = useState(false);
-    const [newMessage, setNewMessage] = useState("");
-    const [socketConnected, setSocketConnected] = useState(false);
-    const [typing, setTyping] = useState(false);
-    const [istyping, setIsTyping] = useState(false);
-    };
+// const SingleChat = ({ fetchAgain, setFetchAgain }) => {
+//     const [messages, setMessages] = useState([]);
+//     const [loading, setLoading] = useState(false);
+//     const [newMessage, setNewMessage] = useState("");
+//     const [socketConnected, setSocketConnected] = useState(false);
+//     const [typing, setTyping] = useState(false);
+//     const [istyping, setIsTyping] = useState(false);
+//     };
 
-export default Group;
+// export default Group;
