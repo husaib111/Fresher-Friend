f9d9bf3ceadf7c40c9b471d5bdeb1bcdae4e6163
TEB018 <TEB018@student.bham.ac.uk>
2022-03-10 16:26:40 +0000

    Added a title to the group page

--- client/src/components/Group/Group.js
+++ client/src/components/Group/Group.js
@@ -7,6 +7,7 @@ function Group() {
 
   return (
       <div>
+          <h1 className="title">Group Name</h1>
           <ProfileList />
           <div className="chat-window-holder">
           <ChatWindow />
