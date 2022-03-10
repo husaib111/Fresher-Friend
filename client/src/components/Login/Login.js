71e9ac4d51f554ea71ff025a4ed540637d6eaf0a
Tomas Grusz <grusztomas0@gmail.com>
2022-03-10 10:15:37 +0100

    Test auth

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -9,7 +9,7 @@ const Login = () => {
   async function loginUser(event) {
     event.preventDefault();
 
-    const response = await fetch("https://localhost:5001/login", {
+    const response = await fetch("http://localhost:5001/login", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
