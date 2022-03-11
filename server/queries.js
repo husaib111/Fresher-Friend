577d79e76eb6d8d3c971e9d9556fb6948c80291b
Husaib Hasan <husaib@hotmail.co.uk>
2022-03-11 16:33:05 +0000

    Added queries for group

--- server/queries.js
+++ server/queries.js
@@ -4,7 +4,6 @@ const pool = require("./dbconnect");
 const getUsers = async (request, response) => {
   try {
     const allUsers = await pool.query("SELECT * FROM users;");
-    console.log(allUsers);
     response.json(allUsers.rows);
   } catch (e) {
     console.log(e.message);
@@ -36,8 +35,55 @@ const getUsersByAccommodation = async (request, response) => {
   }
 };
 
+const getUserBasicInfo = async (request, response) => {
+  try {
+    const { userId } = request.params;
+    console.log(userId);
+    const users = await pool.query(
+      "select first_name,middle_name,last_name,course_name,flat_num,block_num,acc_location from users natural join accommodation natural join courses where user_id=$1",
+      [userId]
+    );
+
+    response.json(users.rows);
+  } catch (e) {
+    console.log(e.message);
+  }
+};
+
+  const getUsersInBlockGroup = async (request, response) => {
+    try {
+      const { userId } = request.params;
+      console.log(userId);
+      const users = await pool.query(
+        "select first_name from users  where block_num = $1",
+        [block_num]
+      );
+  
+      response.json(users.rows);
+    } catch (e) {
+      console.log(e.message);
+    }
+  };
+
+
+    const getUsersInCourseGroup = async (request, response) => {
+      try {
+        const { userId } = request.params;
+        console.log(userId);
+        const users = await pool.query(
+          "select first_name from users  where courseId = $1",
+          [courseId]
+        );
+    
+        response.json(users.rows);
+      } catch (e) {
+        console.log(e.message);
+      }    
+    };
+
 module.exports = {
   getUsers,
   getUsersByCourse,
   getUsersByAccommodation,
+  getUserBasicInfo,
 };
