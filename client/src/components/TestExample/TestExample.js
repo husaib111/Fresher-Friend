0dbdee580d5b1c480464c63a417a104260127dd3
Tomas Grusz <grusztomas0@gmail.com>
2022-03-11 23:07:30 +0100

    Quickfix build

--- client/src/components/TestExample/TestExample.js
+++ client/src/components/TestExample/TestExample.js
@@ -14,6 +14,7 @@ const TestExample = () => {
       }
     );
 
+    console.log(response);
     //const data = response.data;
     //const label = document.getElementsByClassName("fetchData");
     //label.innerHTML = data;
