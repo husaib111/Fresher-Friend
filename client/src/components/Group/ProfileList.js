95fc6bb74f2bb408bf1bb5a4d7fbc168cc528245
Tomas Grusz <grusztomas0@gmail.com>
2022-03-12 16:47:02 +0100

    Change ProfileList request url

--- client/src/components/Group/ProfileList.js
+++ client/src/components/Group/ProfileList.js
@@ -1,56 +1,50 @@
-import "./ProfileList.css"
+import "./ProfileList.css";
 import Axios from "axios";
-import React, {useState, useEffect, Component} from 'react';
+import React, { useState, useEffect, Component } from "react";
 
-import ProfileButton from './ProfileButton';
-function makeProfileButton(name){
-  return(<ProfileButton name={name}/>);
+import ProfileButton from "./ProfileButton";
+function makeProfileButton(name) {
+  return <ProfileButton name={name} />;
 }
 
-function ProfileList(){
-    const getInfo = async (i) => {
-      await Axios.get(
-        "http://localhost:5001/userInfo/"+i,
-        {
-        },
-        {
-          withCredentials: true,
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      )
-        .then((response) => {
-          const {data} = response; 
-          const {first_name}=data[0];
-          // const divForProfileList = document.getElementByClassname('ProfileList');
-          const buttons=[first_name].map(name=>makeProfileButton(name))
-          setInfo(buttons);
-          console.log(first_name);
-        })
-        .catch((e) => {
-          console.log(e);
-        });
-    }
-  
-    const [info,setInfo] = useState(()=>getInfo(2));  
-    useEffect(() => {
-      getInfo(2);
-    },[]); 
+function ProfileList() {
+  const getInfo = async (i) => {
+    await Axios.get("http://www.fresher-friend.bham.team:5001/courseUsers", {
+      withCredentials: true,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => {
+        const { data } = response;
+        const { first_name } = data[0];
+        // const divForProfileList = document.getElementByClassname('ProfileList');
+        const buttons = [first_name].map((name) => makeProfileButton(name));
+        setInfo(buttons);
+        console.log(first_name);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
 
-    return(
+  const [info, setInfo] = useState(() => getInfo(2));
+  useEffect(() => {
+    getInfo(2);
+  }, []);
 
-        <div className="ProfileList">
-          {info[0]}
-            {/* <ProfileButton /> */}
-            {/* <ProfileButton /> */}
-            {/* <ProfileButton /> */}
-            {/* <ProfileButton /> */}
-            {/* <ProfileButton /> */}
-            {/* <ProfileButton /> */}
-            {/* <ProfileButton /> */}
-        </div>
-        )
+  return (
+    <div className="ProfileList">
+      {info[0]}
+      {/* <ProfileButton /> */}
+      {/* <ProfileButton /> */}
+      {/* <ProfileButton /> */}
+      {/* <ProfileButton /> */}
+      {/* <ProfileButton /> */}
+      {/* <ProfileButton /> */}
+      {/* <ProfileButton /> */}
+    </div>
+  );
 }
 
 export default ProfileList;
