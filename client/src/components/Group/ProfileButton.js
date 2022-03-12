bfae6f0a63f7363e870b4f92c8a0b5b69d9416e4
Tomas Grusz <grusztomas0@gmail.com>
2022-03-12 16:32:20 +0100

    Quickfix ProfileButton

--- client/src/components/Group/ProfileButton.js
+++ client/src/components/Group/ProfileButton.js
@@ -1,59 +1,52 @@
 import "./ProfileButton.css";
 import "./Group.css";
-import React, {useState, useEffect, Component} from 'react';
-import {IoPersonCircle} from "react-icons/io5";
+import React, { useState, useEffect } from "react";
+import { IoPersonCircle } from "react-icons/io5";
 import Axios from "axios";
 
-function ProfileButton(){
-  
-    const getInfo = async (i) => {
-      await Axios.get(
-        "http://localhost:5001/userInfo/"+i,
-        {
+function ProfileButton() {
+  const getInfo = async (i) => {
+    await Axios.get(
+      "http://localhost:5001/userInfo/" + i,
+      {},
+      {
+        withCredentials: true,
+        headers: {
+          "Content-Type": "application/json",
         },
-        {
-          withCredentials: true,
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      )
-        .then((response) => {
-          const {data} = response; 
-          const {first_name}=data[0];
-          setInfo([first_name]);
-          console.log(first_name);
-        })
-        .catch((e) => {
-          console.log(e);
-        });
-    }
-  
-    const [info,setInfo] = useState(()=>getInfo(1));  
-    useEffect(() => {
-      getInfo(1);
-    },[]); 
+      }
+    )
+      .then((response) => {
+        const { data } = response;
+        const { first_name } = data[0];
+        setInfo([first_name]);
+        console.log(first_name);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
 
-    return(
-      <div className="ProfileButton">
-          <a href={"/myAccount"}>
-          <div className="ProfileButtonCircle">
-              <IoPersonCircle className={"ProfileButtonIcon"} />
-          </div>
-          <div className="ProfileButtonTitle">
-              <h1>{info[0]}</h1>
-          </div>
-          </a>
-      </div>
-  )
+  const [info, setInfo] = useState(() => getInfo(1));
+  useEffect(() => {
+    getInfo(1);
+  }, []);
+
+  return (
+    <div className="ProfileButton">
+      <a href={"/myAccount"}>
+        <div className="ProfileButtonCircle">
+          <IoPersonCircle className={"ProfileButtonIcon"} />
+        </div>
+        <div className="ProfileButtonTitle">
+          <h1>{info[0]}</h1>
+        </div>
+      </a>
+    </div>
+  );
 }
-
 
 //class ProfileButton extends Component{
 //    render(){
-        
-
-
-
 
 export default ProfileButton;
