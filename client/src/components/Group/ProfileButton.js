6a8432b13262a54869d4b9f8d40330f5d62474de
Husaib Hasan <husaib@hotmail.co.uk>
2022-03-12 15:16:34 +0000

    Group users

--- client/src/components/Group/ProfileButton.js
+++ client/src/components/Group/ProfileButton.js
@@ -1,22 +1,59 @@
 import "./ProfileButton.css";
-import React, {Component} from 'react';
+import "./Group.css";
+import React, {useState, useEffect, Component} from 'react';
 import {IoPersonCircle} from "react-icons/io5";
+import Axios from "axios";
 
-class ProfileButton extends Component{
-    render(){
-        return(
-            <div className="ProfileButton">
-                <a href={"/myAccount"}>
-                <div className="ProfileButtonCircle">
-                    <IoPersonCircle className={"ProfileButtonIcon"} />
-                </div>
-                <div className="ProfileButtonTitle">
-                    Example Person
-                </div>
-                </a>
-            </div>
-        )
+function ProfileButton(){
+  
+    const getInfo = async (i) => {
+      await Axios.get(
+        "http://localhost:5001/userInfo/"+i,
+        {
+        },
+        {
+          withCredentials: true,
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      )
+        .then((response) => {
+          const {data} = response; 
+          const {first_name}=data[0];
+          setInfo([first_name]);
+          console.log(first_name);
+        })
+        .catch((e) => {
+          console.log(e);
+        });
     }
+  
+    const [info,setInfo] = useState(()=>getInfo(1));  
+    useEffect(() => {
+      getInfo(1);
+    },[]); 
+
+    return(
+      <div className="ProfileButton">
+          <a href={"/myAccount"}>
+          <div className="ProfileButtonCircle">
+              <IoPersonCircle className={"ProfileButtonIcon"} />
+          </div>
+          <div className="ProfileButtonTitle">
+              <h1>{info[0]}</h1>
+          </div>
+          </a>
+      </div>
+  )
 }
+
+
+//class ProfileButton extends Component{
+//    render(){
+        
+
+
+
 
 export default ProfileButton;
