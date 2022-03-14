bf8f4283d6a0a3100a0ade1e5200c9eba9d240f2
Tomas Grusz <grusztomas0@gmail.com>
2022-03-14 20:42:16 +0100

    Remove PrivacyPolicy from App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import "./App.css";
 import React from "react";
 import Login from "./components/Login/Login";
-import PrivacyPolicy from "./components/Login/PrivacyPolicy";
 import HomePage from "./components/HomePage/HomePage";
 import Account from "./components/Account/Account";
 import Group from "./components/Group/Group";
@@ -23,7 +22,6 @@ class App extends React.Component {
           <Route path="/" element={<Login />} />
           <Route path="/Group" element={<Group />} />
           <Route path="/test" element={<TestExample />} />
-          <Route path="/privacy" element={<PrivacyPolicy />} />
         </Routes>
       </Router>
     );
