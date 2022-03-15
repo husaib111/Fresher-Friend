cd4185c5bae4b00b2323d1cce60716a11fcc587b
Tomas Grusz <grusztomas0@gmail.com>
2022-03-15 10:34:10 +0100

    Add login backend test

--- server/test.js
+++ server/test.js
@@ -16,4 +16,21 @@ describe("Server tests", () => {
 
     expect(response.statusCode).toBe(200);
   });
+
+  test("Login test", async () => {
+    const json = {
+      body: {
+        email: "txg071@student.bham.ac.uk",
+        password: "MySecurePassword",
+      },
+      withCredentials: true,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    const response = await request(server).post("/login").send(json);
+
+    console.log(response.body);
+    expect(response.statusCode).toBe(200);
+  });
 });
