273427106028bbc8f2fbe55cfe2b40af6d2d0e85
TEB018 <TEB018@student.bham.ac.uk>
2022-03-16 15:57:01 +0000

    Adding Navbar to homepage

--- client/src/components/HomePage/HomePage.js
+++ client/src/components/HomePage/HomePage.js
@@ -1,17 +1,16 @@
-import { GiThreeFriends } from "react-icons/gi";
 import GroupsList from "./GroupsList";
 import React from "react";
+import Navbar from "../Navbar/Navbar";
 
 class HomePage extends React.Component {
   render() {
     return (
+        <div>
+            <Navbar />
       <div className="HomePage">
-        <h1 className="title">
-          <GiThreeFriends className="icon" />
-          Welcome to Fresher Friend.
-        </h1>
         <GroupsList />
       </div>
+        </div>
     );
   }
 }
