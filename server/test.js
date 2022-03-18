1f5570bccf2eccfb6d0ed7fff60a88afd69f257b
Tomas Grusz <grusztomas0@gmail.com>
2022-03-18 12:08:07 +0100

    Backend test login

--- server/test.js
+++ server/test.js
@@ -1,8 +1,10 @@
 const request = require("supertest");
 const app = require("./index");
 
-const server = app.listen(5002, () => {
-  console.log("Server started on port 5002");
+const PORT = 5002;
+
+const server = app.listen(PORT, () => {
+  console.log("Server started on port %d", PORT);
 });
 
 describe("Server tests", () => {
@@ -19,23 +21,22 @@ describe("Server tests", () => {
     const users = await request(server).get("/test1");
     console.log(users.body);
     expect(users.body.success);
-    // expect();
   });
 
-  // test("Login test", async () => {
-  //   const json = {
-  //     body: {
-  //       email: "txg071@student.bham.ac.uk",
-  //       password: "MySecurePassword",
-  //     },
-  //     withCredentials: true,
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //     },
-  //   };
-  //   const response = await request(server).post("/login").send(json);
+  test("Login test", async () => {
+    const json = {
+      body: {
+        email: "txg071@student.bham.ac.uk",
+        password: "MySecurePassword",
+      },
+      withCredentials: true,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    const response = await request(server).post("/login").send(json);
 
-  //   console.log(response.body);
-  //   expect(response.statusCode).toBe(200);
-  // });
+    console.log(response.body);
+    expect(response.statusCode).toBe(200);
+  });
 });
