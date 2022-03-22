9258b1d01f59ffc036565cc2080b76f542711582
Tomas Grusz <grusztomas0@gmail.com>
2022-03-22 14:24:53 +0100

    Frontend tests check

--- client/src/components/Group/Group.test.js
+++ client/src/components/Group/Group.test.js
@@ -1,26 +1,28 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
+// import React from "react";
+// import { render, screen } from "@testing-library/react";
 import App from "./Group";
-import userEvent from "@testing-library/user-event";
+// import userEvent from "@testing-library/user-event";
 import "@testing-library/jest-dom";
+import { render } from "@testing-library/react";
 
 it("shows correct time for message input", () => {
-  try {
-    jest.useFakeTimers();
-    jest.setSystemTime(new Date("20 Aug 2020 02:12:00 GMT").getTime());
-    render(<App />);
-    // async (dispatch) => {
-    //   await userEvent.type(
-    //     screen.getByLabelText("chatInput"),
-    //     "Chat Window Test"
-    //   );
-    //   const buttonE1 = screen.getByLabelText("chatButton");
-    //   await userEvent.click(buttonE1);
-    //   expect(screen.getByText("02:12")).toBeInTheDocument();
-    // };
-  } finally {
-    jest.useRealTimers();
-  }
+  render(<App />);
+  // try {
+  //   jest.useFakeTimers();
+  //   jest.setSystemTime(new Date("20 Aug 2020 02:12:00 GMT").getTime());
+  //   render(<App />);
+  //   // async (dispatch) => {
+  //   //   await userEvent.type(
+  //   //     screen.getByLabelText("chatInput"),
+  //   //     "Chat Window Test"
+  //   //   );
+  //   //   const buttonE1 = screen.getByLabelText("chatButton");
+  //   //   await userEvent.click(buttonE1);
+  //   //   expect(screen.getByText("02:12")).toBeInTheDocument();
+  //   // };
+  // } finally {
+  //   jest.useRealTimers();
+  // }
 });
 /*
 it("shows the message on screen that a user has input", () => {
