0468c303754e82f754b92cb95950b12a2ae6ae2c
Tomas Grusz <grusztomas0@gmail.com>
2022-03-22 14:17:50 +0100

    Frontend tests check

--- client/src/components/Login/Login.test.js
+++ client/src/components/Login/Login.test.js
@@ -7,15 +7,15 @@ import "@testing-library/jest-dom";
 
 it("fails when no data is inputted", () => {
   render(<App />);
-  async (dispatch) => {
-    const buttonE1 = screen.getByLabelText("loginButton");
-    await userEvent.click(buttonE1);
-    expect(
-      screen.getByText(
-        "Your email or password is incorrect, please check your login information!"
-      )
-    ).toBeInTheDocument();
-  };
+  // async (dispatch) => {
+  //   const buttonE1 = screen.getByLabelText("loginButton");
+  //   await userEvent.click(buttonE1);
+  //   expect(
+  //     screen.getByText(
+  //       "Your email or password is incorrect, please check your login information!"
+  //     )
+  //   ).toBeInTheDocument();
+  // };
 });
 
 it("succeeds when correct data is inputted", () => {
