8100bba2ea88ea3621ca5e4da8ed80f1b152b2b0
Tomas Grusz <grusztomas0@gmail.com>
2022-03-24 11:45:00 +0100

    Remove commented imports

--- client/src/index.js
+++ client/src/index.js
@@ -2,12 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-//import Login from "./components/Login/Login"
-//import AccountCreation from "./components/AccountCreation";
 
 ReactDOM.render(
   <React.StrictMode>
-  <App />
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
