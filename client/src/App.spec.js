14cd39b594b170f9d4dac5ddad9ed4c1ec043c40
Tomas Grusz <grusztomas0@gmail.com>
2022-03-24 13:06:18 +0100

    Change App.spec.js tests

--- client/src/App.spec.js
+++ client/src/App.spec.js
@@ -1,20 +1,21 @@
 import App from "./App";
+import Login from "./components/Login/Login";
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
+
 import "@testing-library/jest-dom";
 
 //SUPPRESS WINDOW ALERT ERROR SINCE THEY ARE NOT PART OF JEST-DOM
-window.alert = () => {};
+// window.alert = () => {};
 
 it("System Test 1", () => {
   try {
     //SHOULD RENDER THE LOGIN PAGE AND GET A TEXT "FRESHER FRIEND"
     render(<App />);
     expect(screen.getByText("Fresher Friend")).toBeInTheDocument();
-    console.log("Hi");
 
-    //SHOULD REMAIN ON LOGIN PAGE BECAUSE NO USER INFO WAS TYPED NOR AGREED TO PRIVACY POLICY
+    // SHOULD REMAIN ON LOGIN PAGE BECAUSE NO USER INFO WAS TYPED NOR AGREED TO PRIVACY POLICY
     const loginButton = screen.getByLabelText("loginButton");
     userEvent.click(loginButton);
     expect(screen.getByLabelText("loginButton")).toBeInTheDocument();
@@ -29,9 +30,11 @@ it("System Test 1", () => {
     expect(screen.getByLabelText("loginButton")).toBeInTheDocument();
 
     //SHOULD LOG IN AND SHOW HOMEPAGE
-    // userEvent.click(screen.getByLabelText("checkPrivacy"));
-    // userEvent.click(loginButton);
-    // expect(screen.getByText("Flat 1")).toBeInTheDocument();
+    userEvent.click(screen.getByLabelText("checkPrivacy"));
+    userEvent.click(loginButton);
+    // console.log(window.alert);
+    // console.log(screen.getByLabelText("loginButton"));
+    // expect(screen.getByText("Privacy Policy")).toBeInTheDocument();
   } catch (e) {
     console.log(e.message);
     expect(false).toBe(true);
