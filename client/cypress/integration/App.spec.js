5156b0f2c697dc859ababb4e0d6bb7733defb36f
Tomas Grusz <grusztomas0@gmail.com>
2022-03-24 13:06:54 +0100

    Create cypress e2e testing folder

---
+++ client/cypress/integration/App.spec.js
@@ -0,0 +1,19 @@
+// App.spec.js created with Cypress
+//
+// Start writing your Cypress tests below!
+// If you're unfamiliar with how Cypress works,
+// check out the link below and learn how to write your first test:
+// https://on.cypress.io/writing-first-test
+
+describe("Test", () => {
+  it("Test 1", () => {
+    //login
+    cy.visit("http://localhost:3000");
+    cy.findByRole("textbox", { name: /emailinput/i }).type(
+      "txg071@student.bham.ac.uk"
+    );
+    cy.findByPlaceholderText(/password/i).type("MySecurePassword");
+    cy.findByRole("checkbox", { name: /checkprivacy/i }).click();
+    cy.findByRole("button", { name: /loginbutton/i }).click();
+  });
+});
