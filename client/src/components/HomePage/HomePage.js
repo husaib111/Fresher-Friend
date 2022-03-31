272d064f291faf4a3fbbffff3ad20ee629cc718d
Husaib Hasan <husaib@hotmail.co.uk>
2022-03-31 18:07:09 +0100

    Events tab

--- client/src/components/HomePage/HomePage.js
+++ client/src/components/HomePage/HomePage.js
@@ -1,6 +1,8 @@
 import GroupsList from "./GroupsList";
 import React from "react";
 import Navbar from "../Navbar/Navbar";
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
 
 class HomePage extends React.Component {
   render() {
@@ -9,8 +11,22 @@ class HomePage extends React.Component {
         <Navbar />
         <div className="HomePage">
           <GroupsList />
+          <Tabs>
+            <TabList>
+              <Tab>Upcoming Events</Tab>
+              <Tab>Previous Events</Tab>
+            </TabList>
+            <TabPanel>
+              <h2>Content</h2>
+            </TabPanel>
+            <TabPanel>
+              <h2>Content</h2>
+            </TabPanel>
+          </Tabs>
+
         </div>
       </div>
+      
     );
   }
 }
