de4daebf36c6b90571437b3a326ca4f209bbb1a3
Husaib Hasan <husaib@hotmail.co.uk>
2022-04-15 11:43:41 +0100

    Fixed not loading create event tab

--- client/src/components/HomePage/HomePage.js
+++ client/src/components/HomePage/HomePage.js
@@ -1,14 +1,41 @@
+import "./HomePage.css";
 import GroupsList from "./GroupsList";
 import EventsList from "./EventsList";
-import CreateEvent from "./CreateEvent";
 import React from "react";
 import Navbar from "../Navbar/Navbar";
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
 
+
 //NEED TO BE ABLE TO RETRIEVE THE NAME DYNAMICCALY INSTEAD OF "EVENT"
 //PLACE A EVENTLIST INSIDE THE TABS RATHER THAN BUTTONS
 class HomePage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      eventName: "Name",
+      eventLocation: 2,
+      eventStartDate: 0,
+      eventStartTime: 0
+    };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  }
+//USE THIS FUNCTION TO WORK ON HOW TO SUBMIT THE DATA TO DATABASE
+  handleSubmit(event) {
+    alert('Successfully created event');
+    event.preventDefault();
+  }
   render() {
     return (
       <div>
@@ -28,7 +55,48 @@ class HomePage extends React.Component {
             <TabPanel>
               <h2>Content</h2>
             </TabPanel>
-            <TabPanel><CreateEvent></CreateEvent></TabPanel>
+            <TabPanel>
+            <form onSubmit={this.handleSubmit}>
+        <label>
+          Event Name: 
+          <input
+            name="eventName"
+            type="string"
+            value={this.state.eventName}
+            onChange={this.handleInputChange} />
+        </label>
+        <br /><br />
+        <label>
+          Event Location: 
+          <input
+            name="eventLocation"
+            type="number"
+            value={this.state.eventLocation}
+            onChange={this.handleInputChange} />
+        </label>
+        <br /><br />
+        <label>
+          Event Start Date: 
+          <input
+            name="eventStartDate"
+            type="date"
+            value={this.state.eventStartDate}
+            onChange={this.handleInputChange} />
+        </label>
+        <br /><br />
+        <label>
+          Event Start Time: 
+          <input
+            name="eventStartTime"
+            type="time"
+            value={this.state.eventStartTime}
+            onChange={this.handleInputChange} />
+        </label>
+        <br /><br />
+        <input className="EventButtonCircle" type="submit" value="Create" />
+      </form>
+              
+            </TabPanel>
           </Tabs>
           </div>
 
