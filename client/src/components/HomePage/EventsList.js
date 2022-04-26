21105207521e43bcf454bf371d5aa662d1cbb697
reubenjsinclair <reubenjsinclair@gmail.com>
2022-04-26 15:23:52 +0000

    Fix useParams undefined

--- client/src/components/HomePage/EventsList.js
+++ client/src/components/HomePage/EventsList.js
@@ -25,7 +25,6 @@ function makeEventButton(name) {
 }
 
 function EventsList(props) {
-  let params = useParams();
   const getInfo = useCallback(async () => {
     await Axios.get(
       "https://www.fresher-friend.bham.team:5001/"+ params.type + "Users",
