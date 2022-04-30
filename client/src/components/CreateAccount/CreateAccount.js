a7974586be8c7f94563d7429d320084b39e0716a
Tomas Grusz <grusztomas0@gmail.com>
2022-04-30 18:29:11 +0200

    Fix error inside CreateAccount.js

--- client/src/components/CreateAccount/CreateAccount.js
+++ client/src/components/CreateAccount/CreateAccount.js
@@ -1,6 +1,6 @@
 import "./CreateAccount.css";
 import React, { useState } from "react";
-//import Axios from "axios";
+import Axios from "axios";
 
 const CreateAccount = () => {
   const [firstName, setFirstName] = useState("");
@@ -15,80 +15,93 @@ const CreateAccount = () => {
   const [course, setCourse] = useState("");
 
   const [checked, setChecked] = useState(false);
-  
+
   const handleChange = () => {
     setChecked(!checked);
   };
-  
-  
+
   async function loginUser(event) {
     event.preventDefault();
 
-     
-      
-		if(password1 === "" || firstName==="" || surname ==="" || email ==="" || flat === "" || block===""){
-			alert("Empty Fields");
-		}
-	    else {
-	    if(!/[A-Za-z]+/.test(firstName)||!/[A-Za-z]*/.test(middleName)||!/[A-Za-z]+/.test(surname)||!/[0-9]+/.test(flat)||!/[0-9]+/.test(block)){
-			alert("Some fields contain invalid characters. Names must be comprised only of letters, and flat and block choices must contain only numbers.");
-		}
-		else{
-		if (!/[a-z][a-z][a-z][0-9][0-9][0-9]@student.bham.ac.uk/.test(email)){
-			alert("Invalid email. Email must be composed of three lowercase letters, followed by 3 numbers, ending with \"@student.bham.ac.uk\"");
-		}
-		else{
-		if (accom === "0"){
-			alert("Please choose an accommodation.");
-		}
-		else{
-		if (course === "0"){
-			alert("Please choose a course.");
-		}
-		else{
-			if (password1 !== password2)
-		  {
-			  alert("Passwords don't match");
-		  }
-		else{
-			if (!checked) {
-      alert("Before you can proceed, you need to agree to the Privacy Policy.");
-			}
-		else{
-			const response = await Axios.post(
-				"https://www.fresher-friend.bham.team:5001/createAccount",
-				{
-					email: email,
-					password: password1,
-					firstName: firstName,
-					middleName: middleName,
-					lastName, lastName,
-					courseId: course,
-					accId: accom,
-				},
-				{
-				withCredentials: true,
-				headers: {
-            "Content-Type": "application/json",
-          },
-		  if (response.status === 201){
-			alert("Account successfully created);
-            return <Redirect to "/login/" />
-		  }
-		  else{
-			alert("An error has occurred. Please check that your credentials are correct and that your email is not already in use.");  
-		  }
+    if (
+      password1 === "" ||
+      firstName === "" ||
+      surname === "" ||
+      email === "" ||
+      flat === "" ||
+      block === ""
+    ) {
+      alert("Empty Fields");
+    } else {
+      if (
+        !/[A-Za-z]+/.test(firstName) ||
+        !/[A-Za-z]*/.test(middleName) ||
+        !/[A-Za-z]+/.test(surname) ||
+        !/[0-9]+/.test(flat) ||
+        !/[0-9]+/.test(block)
+      ) {
+        alert(
+          "Some fields contain invalid characters. Names must be comprised only of letters, and flat and block choices must contain only numbers."
+        );
+      } else {
+        if (!/[a-z][a-z][a-z][0-9][0-9][0-9]@student.bham.ac.uk/.test(email)) {
+          alert(
+            'Invalid email. Email must be composed of three lowercase letters, followed by 3 numbers, ending with "@student.bham.ac.uk"'
+          );
+        } else {
+          if (accom === "0") {
+            alert("Please choose an accommodation.");
+          } else {
+            if (course === "0") {
+              alert("Please choose a course.");
+            } else {
+              if (password1 !== password2) {
+                alert("Passwords don't match");
+              } else {
+                if (!checked) {
+                  alert(
+                    "Before you can proceed, you need to agree to the Privacy Policy."
+                  );
+                } else {
+                  const response = await Axios.post(
+                    "https://www.fresher-friend.bham.team:5001/createAccount",
+                    {
+                      email: email,
+                      password: password1,
+                      firstName: firstName,
+                      middleName: middleName,
+                      lastName: surname,
+                      courseId: course,
+                      accId: accom,
+                    },
+                    {
+                      withCredentials: true,
+                      headers: {
+                        "Content-Type": "application/json",
+                      },
+                    }
+                  );
+                  if (response.status === 201) {
+                    alert("Account successfully created");
+                    window.location.href = "/";
+                  } else {
+                    alert(
+                      "An error has occurred. Please check that your credentials are correct and that your email is not already in use."
+                    );
+                  }
+                }
+              }
+            }
+          }
         }
-      );
-       
-		}
-		}}}}}}
+      }
     }
-  
+  }
+
   return (
-      <div>
-          <h1 className="title">Create Your Account</h1>
-		<form className="loginForm" onSubmit={loginUser}>
+    <div>
+      <h1 className="title">Create Your Account</h1>
+      <form className="loginForm" onSubmit={loginUser}>
         <div className="mb-3 inputDiv">
           <input
             aria-label="firstNameInput"
@@ -98,7 +111,7 @@ const CreateAccount = () => {
             placeholder="First Name"
           />
         </div>
-		<div className="mb-3 inputDiv">
+        <div className="mb-3 inputDiv">
           <input
             aria-label="middleNameInput"
             className="form-control loginInput"
@@ -116,7 +129,7 @@ const CreateAccount = () => {
             placeholder="Surname"
           />
         </div>
-		<div className="mb-3 inputDiv">
+        <div className="mb-3 inputDiv">
           <input
             aria-label="emailInput"
             className="form-control loginInput"
@@ -125,7 +138,7 @@ const CreateAccount = () => {
             placeholder="Email Address"
           />
         </div>
-		<div className="mb-3 inputDiv">
+        <div className="mb-3 inputDiv">
           <input
             aria-label="passwordInput1"
             className="form-control loginInput"
@@ -135,17 +148,17 @@ const CreateAccount = () => {
             placeholder="Password"
           />
         </div>
-		<div className="mb-3 inputDiv">
+        <div className="mb-3 inputDiv">
           <input
             aria-label="passwordInput2"
             className="form-control loginInput"
-			type="password"
+            type="password"
             value={password2}
             onChange={(e) => setPassword2(e.target.value)}
             placeholder="Confirm Password"
           />
         </div>
-		<div className="mb-3 inputDiv">
+        <div className="mb-3 inputDiv">
           <input
             aria-label="flatInput"
             className="form-control loginInput"
@@ -154,7 +167,7 @@ const CreateAccount = () => {
             placeholder="Flat Number"
           />
         </div>
-		<div className="mb-3 inputDiv">
+        <div className="mb-3 inputDiv">
           <input
             aria-label="blockInput"
             className="form-control loginInput"
@@ -163,41 +176,61 @@ const CreateAccount = () => {
             placeholder="Block"
           />
         </div>
-		Accomodation:&emsp;
-		<select class="dropdown" value={accom} onChange={(e) => setAccom(e.target.value)}>
-        <option class="dropdown" value="0">Choose your accommodation</option>
-        <option class="dropdown" value="1">Jarratt</option>
-        <option class="dropdown" value="2">demo</option>
+        Accomodation:&emsp;
+        <select
+          class="dropdown"
+          value={accom}
+          onChange={(e) => setAccom(e.target.value)}
+        >
+          <option class="dropdown" value="0">
+            Choose your accommodation
+          </option>
+          <option class="dropdown" value="1">
+            Jarratt
+          </option>
+          <option class="dropdown" value="2">
+            demo
+          </option>
         </select>
-		<p>
-		Course:&emsp;
-		<select class="dropdown" value={course} onChange={(e) => setCourse(e.target.value)}>
-        <option class="dropdown" value="0">Choose your course</option>
-        <option class="dropdown" value="1">Computer Science</option>
-        <option class="dropdown" value="2">demo</option>
-        </select>
-		</p>
-		<p>
-		Interests
-        <label className="check-container">
-          <input
-            aria-label="checkPrivacy"
-            className="checkbox"
-            type="checkbox"
-            //checked={checked}
-            //onChange={handleChange}
-          />
-          Demo Interest
-        </label>
-		</p>
-		<p>
-		In order to register to FresherFriend, you must agree to the &nbsp;
-		<a data-testid="privacy policy Link" href="/privacy">
+        <p>
+          Course:&emsp;
+          <select
+            class="dropdown"
+            value={course}
+            onChange={(e) => setCourse(e.target.value)}
+          >
+            <option class="dropdown" value="0">
+              Choose your course
+            </option>
+            <option class="dropdown" value="1">
+              Computer Science
+            </option>
+            <option class="dropdown" value="2">
+              demo
+            </option>
+          </select>
+        </p>
+        <p>
+          Interests
+          <label className="check-container">
+            <input
+              aria-label="checkPrivacy"
+              className="checkbox"
+              type="checkbox"
+              //checked={checked}
+              //onChange={handleChange}
+            />
+            Demo Interest
+          </label>
+        </p>
+        <p>
+          In order to register to FresherFriend, you must agree to the &nbsp;
+          <a data-testid="privacy policy Link" href="/privacy">
             Privacy Policy
           </a>
-		 </p>
-		<p>
-		<input
+        </p>
+        <p>
+          <input
             aria-label="By ticking, I acknowledge and accept FresherFriend's privacy policy"
             className="checkbox"
             type="checkbox"
@@ -206,15 +239,15 @@ const CreateAccount = () => {
           />
           By continuing, I acknowledge and accept FresherFriend’s Privacy Policy
         </p>
-		<input
+        <input
           aria-label="createAccountButton"
           type="submit"
           value="Create Account"
           className="loginButton btn btn-primary"
         ></input>
-	  </form>
-	  </div>
-    );
+      </form>
+    </div>
+  );
 };
 
 export default CreateAccount;
