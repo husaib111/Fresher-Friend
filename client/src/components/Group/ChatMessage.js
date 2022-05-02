40bc8aa36b5bb8438b7a7dae4841cf2149afaab0
TEB018 <TEB018@student.bham.ac.uk>
2022-05-02 01:16:23 +0100

    Make chat messages look neater

--- client/src/components/Group/ChatMessage.js
+++ client/src/components/Group/ChatMessage.js
@@ -4,14 +4,18 @@ import "./ChatMessage.css"
 
 
 function ChatMessage(props) {
+
+    const newDate = props.time.split('T');
+    const formattedTime = newDate.length >= 1 ? newDate[1].slice(0, 8) : '';
+
     return (
-        <div className={`ChatMessage ${props.author===props.user? "you": "other"}`}>
-                <div>
-                <div className="MessageInfo">{props.author} - {props.time}  </div>
+        <div className={`ChatMessage ${props.type}`}>
+
+                <div className="MessageInfo">{props.author} - {formattedTime}  </div>
                 <div className="MessageBubble">
                     <div className="messageText">{props.text}</div>
                 </div>
-                </div>
+
         </div>
     );
 }
