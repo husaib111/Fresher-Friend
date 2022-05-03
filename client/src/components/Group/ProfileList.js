6948f96c797543d8728acf2789995a782d0cb77a
reubenjsinclair <reubenjsinclair@gmail.com>
2022-05-03 13:29:38 +0000

    Fix group page profiles

--- client/src/components/Group/ProfileList.js
+++ client/src/components/Group/ProfileList.js
@@ -3,30 +3,10 @@ import "./ProfileButton.css";
 import "./Group.css";
 import Axios from "axios";
 import React, { useCallback, useState, useEffect } from "react";
-import { IoPersonCircle } from "react-icons/io5";
 import { useParams } from "react-router-dom";
 
-function ProfileButton(props) {
-  return (
-    <div className="ProfileButton">
-      <a href={"/account/" + props.username}>
-        <div className="ProfileButtonCircle">
-          <IoPersonCircle className={"ProfileButtonIcon"} />
-        </div>
-        <h1 className="ProfileButtonTitle">{props.name}</h1>
-      </a>
-    </div>
-  );
-}
-
-function makeProfileButton(name) {
-  console.log(name);
-  const { first_name, email } = name;
-  const username = email.substring(0, email.lastIndexOf("@"));
-  return <ProfileButton name={first_name} username={username} />;
-}
-
 function ProfileList(props) {
+  const [pfs, setPfs] = useState([]);
   let params = useParams();
   const getInfo = useCallback(async () => {
     await Axios.get(
@@ -41,19 +21,80 @@ function ProfileList(props) {
       .then((response) => {
         const { data } = response;
         // console.log(data);
-        const buttons = data.map((name) => makeProfileButton(name));
+        const buttons = data.map((name, i) => makeProfileButton(name, i));
         console.log(buttons);
         setInfo(buttons);
       })
       .catch((e) => {
         console.log(e);
       });
-  },[params.type]);
+  }, [params.type]);
+
+  const getPfs = useCallback(async () => {
+    await Axios.get("https://www.fresher-friend.bham.team/" + params.type + "Users", {
+      withCredentials: true,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => {
+        const { data } = response;
+        // console.log(data);
+        data.map(async (name) => {
+          const {  email } = name;
+          const username = email.substring(0, email.lastIndexOf("@"));
+
+          await Axios.get("https://www.fresher-friend.bham.team/profile/" + username, {
+            withCredentials: true,
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }).then((response) => {
+            console.log(response);
+            const { data } = response;
+            const { filename } = data;
+              console.log("pfs:" + pfs);
+            setPfs(pfs.push(filename));
+          });
+        });
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }, [pfs, params.type]);
 
   const [info, setInfo] = useState([]);
   useEffect(() => {
+    getPfs();
     getInfo();
-  },[getInfo]);
+  }, [getInfo,getPfs]);
+
+  function ProfileButton(props) {
+    console.log(pfs[props.add]);
+    return (
+      <div className="ProfileButton">
+        <a href={"/account/" + props.username}>
+          <div className="ProfileButtonCircle">
+            <img
+		alt="Profile"
+              src={"https://www.fresher-friend.bham.team/" + pfs[props.add]}
+              className={"ProfileButtonIcon"}
+            />
+          </div>
+          <h1 className="ProfileButtonTitle">{props.name}</h1>
+        </a>
+      </div>
+    );
+  }
+
+  function makeProfileButton(name, i) {
+    console.log(name);
+    const { first_name, email } = name;
+    const username = email.substring(0, email.lastIndexOf("@"));
+    return (
+      <ProfileButton name={first_name} username={username} key={i} add={i} />
+    );
+  }
 
   return <div className="ProfileList">{info}</div>;
 }
